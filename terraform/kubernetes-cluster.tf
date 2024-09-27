fbe7720574056af1b0712fe8451f9a9ec00c7276
srii03 <162790549+srii03@users.noreply.github.com>
2024-09-27 16:44:02 +1000

    Update kubernetes-cluster.tf

--- terraform/kubernetes-cluster.tf
+++ terraform/kubernetes-cluster.tf
@@ -1,23 +1,20 @@
-# Create an Azure Kubernetes Service cluster
 resource "azurerm_kubernetes_cluster" "example" {
   name                = var.cluster_name
-  location            = "Australia Southeast"  # Your location
-  resource_group_name = "deakinuni"  # Your resource group name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
 
-  sku {
-    name     = "Basic"
-    tier     = "Free"
-    capacity = 1
-  }
-
-  agent_pool_profile {
-    name       = "agentpool"
-    count      = 1
+  default_node_pool {
+    name       = "default"
+    node_count = 1
     vm_size    = "Standard_DS2_v2"
-    os_type    = "Linux"
   }
 
   identity {
     type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
   }
 }
